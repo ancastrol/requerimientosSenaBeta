713faefe31d0f92b1be9fc1d717c9b6680123c81
Andree2208 <andres_fcastrol@soy.sena.edu.co>
2024-08-28 11:25:55 -0500

    Filtro B1...B12
    Se realiza iteración para que revise la entrega de las 12 bitacoras y se adapte a la fecha en la que envia la notificación

--- correo.py
+++ correo.py
@@ -13,8 +13,8 @@ df.columns = df.columns.str.strip()
 # Obtener la fecha actual
 fecha_actual = datetime.now().date()
 
-# Número de días después de los cuales se debe enviar el correo electrónico
-dias_umbral = 15
+# Número de días entre bitacoras
+intervalo_dias = 15
 
 for index, row in df.iterrows():
 
@@ -22,71 +22,66 @@ for index, row in df.iterrows():
     if pd.isna(row).all():
         print("Se encontró una fila vacía, deteniendo la iteración.")
         break
-
-    # Imprimir los valores de la fila actual
-    print(f"Procesando fila {index}:")
     
-    # Buscar la columna que contiene la fecha de inicio
+    # Buscar las columnas que contienen los datos necesario para realizar los filtros
+    # Fecha en la que se inicio la etapa productiva
     fecha_columna = [col for col in df.columns if 'Inicio_Ficha(Productiva)' in col]
+    # Columna B1
     columna_b1 = [col for col in df.columns if 'B1' in col]
+    # Correo del aprendiz
     correo_aprendiz = [col for col in df.columns if 'CorreoAprendiz' in col]
-    print(f"Columnas encontradas: {correo_aprendiz}")
 
+    # Se verifica la fecha de inicio de la etapa productiva y se convierte a tipo dateTime
     if fecha_columna:
         fecha_columna = df[fecha_columna[0]].iloc[0]
-        print(f"Columna de fecha encontrada: {fecha_columna}")
         fecha_inicio = pd.to_datetime(fecha_columna).date()
-
     else:
         print("No se encontró una columna de fecha adecuada")
         break
-    
-    # Calcular la diferencia en días entre la fecha actual y la fecha en la base de datos
-    diferencia_dias = (fecha_actual - fecha_inicio).days
-    print(f"Diferencia en días: {diferencia_dias}")
-    
-    # Verificar si han pasado al menos los días de umbral
-    if diferencia_dias >= dias_umbral:
-        
-        # Sacar el valor de la columna B1
-        valor_b1 = df[columna_b1[0]].iloc[0]
 
-        # Verificar si la casilla específica tiene el valor "si"
-        if valor_b1 != 'si':
+    # Iterar sobre las columnas de bitacora para verificar cual se ha enviado
+    for i in range(1, 13):
+        columna_bitacora = f'B{i}'
 
+        # Calcular la fecha en la que se debe enviar la notificación
+        fecha_notificacion = fecha_inicio + timedelta(days=intervalo_dias * i)
+
+        # Verificar si subio la bitacora
+        estado_bitacora = df[columna_bitacora].iloc[0].strip().lower()
+
+        # Verificar si se debe enviar la notificación
+        if estado_bitacora == 'no' and fecha_notificacion <= fecha_actual:
             # Obtener el destinatario del correo electrónico
-            destinatario = df[correo_aprendiz[0]].iloc[0]
+                destinatario = df[correo_aprendiz[0]].iloc[0]
 
-            # Obtener los detalles del correo electrónico
-            if destinatario:
+                # Obtener los detalles del correo electrónico
+                if destinatario:
 
-                msg = MIMEMultipart()
-                msg['From'] = 'astroc2208@gmail.co'
-                msg['To'] = destinatario
-                msg['Subject'] = 'Falta entrega de bitacora 1'
-                # subject = 'Notificación automática'
-                body = 'Este es un correo electrónico automático enviado para notificarle que ya paso la fecha y no ha hecho entrega de la bitacora #1.'
-                msg.attach(MIMEText(body, 'plain'))
+                    msg = MIMEMultipart()
+                    msg['From'] = 'astroc2208@gmail.co'
+                    msg['To'] = destinatario
+                    msg['Subject'] = f'Falta entrega de bitacora {i}'
+                    body = f'Por medio de este correo se le notifica que la bitacora {i} no ha sido entregada y debio haber sido subida el dia {fecha_notificacion}.'
+                    msg.attach(MIMEText(body, 'plain'))
 
-                # Conexión al servidor SMTP
-                smtp = smtplib.SMTP('smtp.gmail.com', 587)
-                smtp.starttls()
+                    # Conexión al servidor SMTP
+                    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+                    smtp.starttls()
 
-                # Autenticación con tu correo y contraseña de aplicación
-                smtp.login('astroc2208@gmail.com', 'jsgm gpyz gakh ywop')
+                    # Autenticación con tu correo y contraseña de aplicación
+                    smtp.login('astroc2208@gmail.com', 'jsgm gpyz gakh ywop')
 
-                # Envío del correo
-                smtp.send_message(msg)
-                print(f"Correo enviado a {destinatario}")
+                    # Envío del correo
+                    smtp.send_message(msg)
+                    print(f"Correo enviado a {destinatario}")
 
-                # Cierre de la conexión SMTP
-                smtp.quit()
-                
-            else:
-                print("No se encontró un correo electronico registrado")
+                    # Cierre de la conexión SMTP
+                    smtp.quit()
+                    
+                else:
+                    print("No se encontró un correo electronico registrado")
+                break     
         else:
-            print(f"Bitacora 1 esta subida, no es necesario el envió de la notificación")
-    else:
-        print(f"Han pasado {diferencia_dias} días, no se ha superado el umbral de {dias_umbral} días")
+            print(f'{columna_bitacora} ya fue enviada o el aprendiz tiene tiempo de enviarla.')
 
 print("Proceso completado")
